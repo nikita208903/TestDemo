0e615d6c6b1907e2a4e7e3418e05bc29f92d8b51
IPSNICKS <IPSNICKS@192.168.1.35>
2022-03-01 13:54:29 +0530

    Added some lines of code in GitDemo.java class

--- FirstProject/src/GitDemo.java
+++ FirstProject/src/GitDemo.java
@@ -1,9 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.interactions.Actions;
 
 public class GitDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://github.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.tagName("input")).sendKeys("react"+ Keys.ENTER);
+		driver.findElement(By.linkText("Advanced search")).click();
+		//Actions actions = new Actions(driver);
+		//actions.keyDown(Keys.CONTROL).sendKeys(Keys.END).perform();
+		
+			
 	}
 
 }
